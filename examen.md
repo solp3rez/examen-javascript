995edcb0bfe1875161e2d606f6dadec635c2e796
solp3rez <axelweath@gmail.com>
2025-05-26 15:00:39 -0300

    examen

---
+++ examen.md
@@ -0,0 +1,20 @@
+/*Examen de JavaScript*/
+T-E-O-R-I-C-O
+
+1.
+la diferencia es que let se utiliza para variables que cambian o que se modifica  y const se utiliza para variables que no cambian o se modifican y var se utiliza en toda la funcion donde se declara
+
+2. 
+MAP :se utiliza para cambiar el valor 
+filter:se utiliza para filtrar cosas y obtener resultados fijos
+for:se utiliza para recorrer arrays 
+forEach:se utiliza para recorrer arrays 
+ 
+3.
+para aceder la propiedad de un objeto se utiliza un punto 
+ejemplo:
+
+const estudiante =  { nombre: "Ana", nota: 8 }
+ console.log("mostrar nombres");
+
+
---
+++ javascript.js
@@ -0,0 +1,31 @@
+//PRACTICO 1
+const numeros = [3, 7, 2, 9, 12, 5, 8];
+if(numeros >= 5){
+ console.log("mayores a 5");
+}else if(numeros <= 5){
+    console.log("menores a 5");
+}
+//b) Crear un nuevo arreglo que contenga solamente los números pares.
+
+
+
+
+
+//PRACTICO 2 
+
+
+//  a) Crear un nuevo arreglo que contenga solo los nombres de los estudiantes.
+ 
+//b) Obtener los estudiantes que aprobaron (nota mayor o igual a 6)
+
+const estudiantes = [
+  { nombre: "Ana", nota: 8 },
+  { nombre: "Juan", nota: 5 },
+  { nombre: "Lucía", nota: 9 },
+  { nombre: "Pedro", nota: 4 }];
+
+if( nota >= 6){
+    console.log("aprobados");
+}else if (nota <= 6){
+    console.log("desaprobados");
+}
