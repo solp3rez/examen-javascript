0a76615ba6bd60712452b093bfa8962cfe24c11e
Joaquin Blanco <blancojoaquin77@gmail.com>
2025-11-12 11:11:37 -0300

    terminado y entregado

--- extraccion/src/dia_scraper.py
+++ extraccion/src/dia_scraper.py
@@ -5,46 +5,53 @@ from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 # Configura el WebDriver (usando Firefox)
 def configurar_driver():
     # Opciones del navegador
     firefox_options = Options()
-    firefox_options.add_argument("--headless")  # Ejecutar sin abrir ventana (modo "headless")
-    
+    # firefox_options.add_argument("--headless")  # Ejecutar sin abrir ventana (modo "headless")
+
     # Configura el servicio de geckodriver
-    service = Service('/usr/local/bin/geckodriver')  # La ruta de geckodriver
-    driver = webdriver.Firefox(service=service, options=firefox_options)
+    # service = Service('/usr/local/bin/geckodriver')  # La ruta de geckodriver
+    driver = webdriver.Firefox()
     return driver
+
 
 # Obtener los productos de Coto usando Selenium
 def obtener_productos_dia():
     url = "https://diaonline.supermercadosdia.com.ar/almacen/golosinas-y-alfajores?initialMap=c,c&initialQuery=almacen/golosinas-y-alfajores&map=category-1,category-2,category-3&query=/almacen/golosinas-y-alfajores/alfajores&searchState"
     driver = configurar_driver()
     driver.get(url)
-    
+
     # Esperar que la página cargue
     time.sleep(5)
-    
+
     # Obtener el HTML de la página cargada
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    
+
     # Buscar todos los productos
     productos = soup.find_all("section")
     datos_productos = []
-    
+
     for producto in productos:
         try:
             nombre = producto.find("h3").span.text
-            precio = producto.find("span",{"class":"diaio-store-5-x-sellingPriceValue"}).text
+            precio = producto.find(
+                "span", {"class": "diaio-store-5-x-sellingPriceValue"}
+            ).text
             enlace = "https://www.supermercadosdia.com.ar/" + producto.find("a")["href"]
-            datos_productos.append({"nombre": nombre, "precio": precio, "enlace": enlace})
+            datos_productos.append(
+                {"nombre": nombre, "precio": precio, "enlace": enlace}
+            )
         except Exception as e:
             print(f"Error: {e}")
-    
+
     # Cerrar el navegador
     driver.quit()
-    
+
     return datos_productos
+
 
 # Guardar los productos en un archivo CSV
 def guardar_productos_en_csv(productos):
@@ -55,11 +62,13 @@ def guardar_productos_en_csv(productos):
     else:
         print("❌ No se extrajeron productos")
 
+
 # Función principal
 def main():
     productos = obtener_productos_dia()
     guardar_productos_en_csv(productos)
     print(productos[:5])  # Mostrar los primeros 5 productos
+
 
 if __name__ == "__main__":
     main()
--- extraccion/test/test_se_crean_archivos_csv.py
+++ extraccion/test/test_se_crean_archivos_csv.py
@@ -17,18 +17,19 @@ archivos = [
     "productos_dia.csv",
 ]
 
+
 def test_verificar_si_se_crean_archivos():
     for archivo in archivos:
         directorio = pathlib.Path("src", archivo)
-        assert not directorio.exists() 
+        assert not directorio.exists()
 
     productos = obtener_productos_carrefour()
     guardar_productos_en_csv(productos)
-    obtener_productos_dia(productos)
-    obtener_productos_coto(productos)
-   
+    productos = obtener_productos_dia()
+    guardar_productos_en_csv(productos)
+    productos = obtener_productos_coto()
+    guardar_productos_en_csv(productos)
+
     for archivo in archivos:
         directorio = pathlib.Path("src", archivo)
-        assert not directorio.exists() 
-
-
+        assert not directorio.exists()
